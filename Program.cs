b7643fa7857d89478318252c637db6836cdbc592
diana <danavay324@gmail.com>
2026-07-01 18:50:54 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+namespace KorochkyEst
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+
+            var app = builder.Build();
+
+            app.UseDefaultFiles(); // Заставит сервер искать index.html по умолчанию
+            app.UseStaticFiles();  // Разрешит открывать файлы из wwwroot
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+
+            app.Run();
+        }
+    }
+}
